09fe68c122614c270cdc23f476085a2623f4907a
asinduG <asindugayanganah@gmail.com>
2021-04-18 02:30:39 +0530

    code some crud operations

--- GadgetBadget/src/com/ProductService.java
+++ GadgetBadget/src/com/ProductService.java
@@ -1,5 +1,5 @@
 package com;
-import module.Product;
+import module.Product; 
 
 import java.sql.Date;
 import java.sql.Time;
@@ -34,5 +34,57 @@ public class ProductService {
 			 String output = obPro.addToCart(pid, researcherId, date,time,totAmount,status); 
 			return output; 
 			}
+	
+	@GET
+	@Path("/") 
+	@Produces(MediaType.TEXT_HTML) 
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	
+	
+	public String viewCart() 
+	 { 
+		
+	 return obPro.viewCart();
+	 
+	 }
+	
+	@PUT
+	@Path("/") 
+	@Consumes(MediaType.APPLICATION_JSON) 
+	@Produces(MediaType.TEXT_PLAIN) 
+	
+	public String updateCart(String productData) 
+	{ 
+	//Convert the input string to a JSON object 
+	 JsonObject cartObject = new JsonParser().parse(productData).getAsJsonObject(); 
+	//Read the values from the JSON object
+	 String orderId = cartObject.get("orderId").getAsString(); 
+	 String pid = cartObject.get("pid").getAsString(); 
+	 String researcherId = cartObject.get("researcherId").getAsString(); 
+	 String date = cartObject.get("date").getAsString(); 
+	 String time = cartObject.get("time").getAsString();
+	 String totAmount = cartObject.get("totAmount").getAsString();
+	 String status = cartObject.get("status").getAsString();
+	 
+	 String output = obPro.updateCart(orderId, pid, researcherId, date, time,totAmount,status); 
+	 
+	 return output; 
+	}
+	
+	@DELETE
+	@Path("/") 
+	@Consumes(MediaType.APPLICATION_XML) 
+	@Produces(MediaType.TEXT_PLAIN) 
+	
+	public String deleteCartItem(String cartData) 
+	{ 
+	//Convert the input string to an XML document
+	 Document doc = Jsoup.parse(cartData, "", Parser.xmlParser()); 
+	 
+	//Read the value from the element <itemID>
+	 String orderId = doc.select("orderId").text(); 
+	 String output = obPro.deleteCartItem(orderId); 
+	return output; 
+	}
 
 }
--- GadgetBadget/src/module/Product.java
+++ GadgetBadget/src/module/Product.java
@@ -1,6 +1,7 @@
 package module;
 
-import java.sql.*; 
+import java.sql.*;
+import java.text.SimpleDateFormat; 
  
 public class Product {
 
@@ -18,8 +19,8 @@ public class Product {
 		return con;
 	}
 	
-	public String addToCart(String pid,String researcherId,Date date,Time time,String totAmount,String status) {
-		
+	public String addToCart(String pid,String researcherId,Date date,Time time,String totAmount,String status) 
+	{		
 		String output = "";
 		try {
 			Connection con = connect();
@@ -53,5 +54,125 @@ public class Product {
 		
 		return output;		
 	} 
+	
+	public String viewCart() {
+		
+		String output = "";
+		try {
+			Connection con =  connect();
+			
+			if(con == null) {
+				return "Error while connecting to the database for reading.";
+			}
+			
+			output = "<table border ='1'><tr><td>projectID</td>" +
+					 "<td>date</td>" +
+					 "<td>time</td>" +
+					 "<td>TotalAmount</td>" +
+					 "<td>status</td>" +
+					 "<td>Update</td>" +
+					 "<td>Remove</td></tr>"; 
+			
+			String query = "select * from order";
+			Statement stmt = con.createStatement();
+			ResultSet rs   = stmt.executeQuery(query);
+			
+			while(rs.next()) {
+				String orderId = Integer.toString(rs.getInt("orderId"));
+				String pid = Integer.toString(rs.getInt("pid"));
+				String researcherId = Integer.toString(rs.getInt("researcherId"));
+				SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");  
+			    String date = formatter.format("date"); 
+			    Time timeObj = rs.getTime("time");
+			    String time = timeObj.toString();
+			    String totAmount = Float.toString(rs.getFloat("totAmount"));
+			    String status = rs.getString("status");
+			
+				output += "<tr><td>"+ pid + "</td>";
+				output += "<tr><td>"+ date + "</td>";
+				output += "<tr><td>"+ time + "</td>";
+				output += "<tr><td>"+ totAmount + "</td>";
+				output += "<tr><td>"+ status + "</td>";
+				
+				output += "<td><input name = 'btnUpdate' type = 'button' value = 'Update' class='btn btn-secondary'></td>"
+				+ "<td><form method = 'post' action = 'products.jsp'>"
+				+ "<input name='btnRemove' type = 'button' value = 'Delete' class='btn btn-danger'>"
+				+ "<input name = 'orderId' type = 'hidden' value = '"+orderId+"'>"
+				+ "</form></td></tr>";
+			}
+				con.close();
+				output += "</table>";
+							
+		}
+		catch(Exception e) {
+			output = "Occure error while reading products";
+			System.err.println(e.getMessage()); 
+		}
+		
+		return output;
+		 
+	}
+	
+	public String updateCart(String orderId, String pid,String researcherId,String date,String time,String totAmount,String status) 
+	{
+		String output = "";
+		
+		try 
+		{
+			Connection con = connect();
+			
+			if(con == null) {
+				return "Error while connecting to the database";
+			}
+			
+			String query = "Update order set pid=? ,researcherId=? ,date=? ,time=? ,totAmount=? ,status=? Where orderId=?";
+			PreparedStatement stmt = con.prepareStatement(query);
+			
+			stmt.setInt(1,Integer.parseInt(pid));
+			stmt.setInt(2,Integer.parseInt(researcherId));
+//			stmt.setDate(3,date);
+//			stmt.setTime(4,time);
+			stmt.setFloat(5,Float.parseFloat(totAmount));
+			stmt.setString(6,status);
+			
+			stmt.execute();
+			con.close();
+			output = "Updated successfully";
+			
+		}
+		catch(Exception e) {
+			output = "Occure error while updating products";
+			System.err.println(e.getMessage());
+		}
+		
+		return output;
+	}
+	
+	public String deleteCartItem(String orderId) {
+		
+		String output = "";
+		
+		try {
+			Connection con = connect();
+			
+			if(con == null) {
+				return "Error while connecting to the database.";
+			}
+			
+			String query = "delete from order where orderId=?";
+			PreparedStatement stmt = con.prepareStatement(query);
+			
+			stmt.setInt(1,Integer.parseInt(orderId));
+			
+			stmt.execute();
+			con.close();
+			output = "Product deleted from cart successfully.";
+		}
+		catch(Exception e) {
+			output = "Error while deleting product from cart!!!!.";
+			System.err.println(e.getMessage());
+		}
+		return output;
+	}
 	
 }
