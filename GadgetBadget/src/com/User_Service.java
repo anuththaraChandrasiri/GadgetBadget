d7d771677482bc6f2223ac5854cd24e425d9aff1
Pasan's Pc <pasancbandara@gmail,.com>
2021-04-18 18:08:43 +0530

    User insert method added

--- GadgetBadget/src/com/User_Service.java
+++ GadgetBadget/src/com/User_Service.java
@@ -1,14 +1,13 @@
 package com;
 
+import model.User;
+
 //For REST Service
 import javax.ws.rs.*;
-
 import javax.ws.rs.core.MediaType;
 
 //For JSON
 import com.google.gson.*;
-
-import model.User;
 
 //For XML
 import org.jsoup.*;
@@ -26,6 +25,23 @@ public class User_Service
 	public String readUsers()
 	{
 		return userObj.readUsers();
+	}
+	
+	@POST
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String insertUser(@FormParam("firstName") String firstName,
+		 @FormParam("lastName") String lastName,
+		 @FormParam("userName") String userName,
+		 @FormParam("email") String email,
+		 @FormParam("cardNumber") String cardNumber,
+		 @FormParam("CVV") String CVV,
+		 @FormParam("expDate") String expDate,
+		 @FormParam("password") String password)
+	{
+		 String output = userObj.insertUser(firstName,lastName,userName,email,cardNumber,CVV,expDate,password);
+		return output;
 	}
 
 
--- GadgetBadget/src/model/User.java
+++ GadgetBadget/src/model/User.java
@@ -22,6 +22,7 @@ public class User {
 	 }
 
 	
+	
 	public String readUsers()
 	 {
 		 String output = "";
@@ -35,6 +36,7 @@ public class User {
 		 // Prepare the html table to be displayed
 		 output = "<table border='1'><tr><th>First Name</th><th>Last Name</th>" +
 		 "<th>User Name</th>" +
+		 "<th>Password</th>" +
 		 "<th>Email</th>" +
 		 "<th>Card Number</th>" +
 		 "<th>CVV</th>" +
@@ -56,12 +58,14 @@ public class User {
 			 String CardNumber = rs.getString("CardNumber");
 			 String CVV = rs.getString("CVV");
 			 String expDate = rs.getString("expDate");
+			 String password = rs.getString("password");
 			 
 			 // Add into the html table
-			 output += "<td>" + userName + "</td>";
-			 output += "<td>" + email + "</td>";
-			 output += "<tr><td>" + firstName + "</td>";
+			 output += "<td>" + firstName + "</td>";
 			 output += "<td>" + lastName + "</td>";
+			 output += "<td>" + userName + "</td>";
+			 output += "<td>" + password + "</td>";
+			 output += "<td>" + email + "</td>";
 			 output += "<td>" + CardNumber + "</td>";
 			 output += "<td>" + CVV + "</td>";
 			 output += "<td>" + expDate + "</td>";
@@ -80,10 +84,51 @@ public class User {
 	 }
 	 catch (Exception e)
 	 {
-		 output = "Error while reading the items.";
+		 output = "Error while reading Users.";
 		 System.err.println(e.getMessage());
 	 }
 		 return output;
 	 }
+	
+	
+	
+	public String insertUser(String fname, String lname, String uname, String email,String cnumber,String cvv,String expdate,String password)
+	{
+		String output = "";
+		try
+		{
+			Connection con = connect();
+			if (con == null)
+			{
+				return "Error while connecting to the database";
+			}
+
+			// create a prepared statement
+			String query = " insert into user('firstName','lastName','userName','email','cardNumber','CVV','expDate','password') values (?, ?, ?, ?, ?, ?, ?, ?,?)";
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+
+			// binding values
+			preparedStmt.setString(2, fname);
+			preparedStmt.setString(3, lname);
+			preparedStmt.setString(4, uname);
+			preparedStmt.setString(5, email);
+			preparedStmt.setString(6, cnumber);
+			preparedStmt.setString(7, cvv);
+			preparedStmt.setString(8, expdate);
+			preparedStmt.setString(9, password);
+			
+			//execute the statement
+			preparedStmt.execute();
+			con.close();
+			output = "Inserted successfully";
+		}
+		catch (Exception e)
+		{
+			output = "Error while inserting";
+			System.err.println(e.getMessage());
+		}
+		return output;
+	}
+	
 	
 }
