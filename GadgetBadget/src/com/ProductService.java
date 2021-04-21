356469a20b58af6839859caa42025bc40e91fbf6
asinduG <asindugayanganah@gmail.com>
2021-04-21 14:47:35 +0530

    Add additional comments for both products and product service

--- GadgetBadget/src/com/ProductService.java
+++ GadgetBadget/src/com/ProductService.java
@@ -17,8 +17,10 @@ import org.jsoup.nodes.Document;
 @Path("/Product")
 public class ProductService {
 	
+	//create object for the Product class
 	Product obPro = new Product();
 	
+	//Reading all ordered products
 	@GET
 	@Path("/") 
 	@Produces(MediaType.TEXT_HTML) 
@@ -28,10 +30,10 @@ public class ProductService {
 	public String viewCart() 
 	 { 
 		
-	 return obPro.viewCart();
+		return obPro.viewCart();
 	 
 	 }
-	
+	//Reading ordered products for the specific user 
 	@GET
 	@Path("/CartItem/{orderId}") 
 	@Produces(MediaType.TEXT_HTML) 
@@ -40,20 +42,13 @@ public class ProductService {
 		
 		return obPro.viewSelectCart(orderId);
 	}
-//	
-//	public String viewSelectCart(String orderId) 
-//	 { 
-//		
-//	 return obPro.viewSelectCart(orderId);
-//	 
-//	 
-//	 }
 	
-	
+	//insert order products for the table
 	@POST
 	@Path("/") 
 	@Consumes(MediaType.APPLICATION_FORM_URLENCODED) 
 	@Produces(MediaType.TEXT_PLAIN)
+	
 	public String addToCart( 
 			 @FormParam("pid") String pid, 
 			 @FormParam("researcherId") String researcherId, 
@@ -63,10 +58,10 @@ public class ProductService {
 			 @FormParam("status") String status) 
 			{ 
 			 String output = obPro.addToCart(pid, researcherId, date,time,totAmount,status); 
-			return output; 
+			 return output; 
 			}
 	
-	
+	//Update order products
 	@PUT
 	@Path("/") 
 	@Consumes(MediaType.APPLICATION_JSON) 
@@ -90,6 +85,7 @@ public class ProductService {
 	 return output; 
 	}
 	
+	//Delete order products for specific ordered ID
 	@DELETE
 	@Path("/") 
 	@Consumes(MediaType.APPLICATION_XML) 
@@ -100,10 +96,10 @@ public class ProductService {
 	//Convert the input string to an XML document
 	 Document doc = Jsoup.parse(productData, "", Parser.xmlParser()); 
 	 
-	//Read the value from the element <itemID>
+	//Read the value from the element <orderId >
 	 String orderId = doc.select("orderId").text(); 
 	 String output = obPro.deleteItem(orderId); 
-	return output; 
+	 return output; 
 	}
 
 }
--- GadgetBadget/src/model/Product.java
+++ GadgetBadget/src/model/Product.java
@@ -1,11 +1,12 @@
 package model;
 
-import java.sql.*;
+import java.sql.*;  
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
  
 public class Product {
 
+	//create database connection
 	private Connection connect(){
 		
 		Connection con = null;
@@ -21,6 +22,7 @@ public class Product {
 		return con;
 	}
 
+	//implement insert method to the cart
 	public String addToCart(String pid,String researcherId,String date,Time time,String totAmount,String status) 
 	 { 
 		String output = ""; 
@@ -43,7 +45,7 @@ public class Product {
 	 preparedStmt.setTime(5,time);
 	 preparedStmt.setFloat(6, Float.parseFloat(totAmount) );
 	 preparedStmt.setString(7, status);
-	// execute the statement3
+	// execute the statement
 	 preparedStmt.execute(); 
 	 con.close(); 
 	 output = "Inserted successfully"; 
@@ -51,14 +53,14 @@ public class Product {
 	 } 
 	 catch (Exception e) 
 	 { 
+		 //print error messages if exist
 		 output = "Error while inserting the item."; 
 		 System.err.println(e.getMessage()); 
 	 } 
 	 	return output; 
 	 }
 	
-	
-	
+	//implement display all ordered products 
 	public String viewCart() {
 		
 		String output = "";
@@ -71,6 +73,7 @@ public class Product {
 				
 			}
 			System.out.print("hellofirst");
+			//create table view structure
 			output = "<table border ='1'><tr><th>projectID</th>" +
 					 "<th>date</th>" +
 					 "<th>time</th>" +
@@ -78,35 +81,29 @@ public class Product {
 					 "<th>status</th>" +
 					 "<th>Update</th>" +
 					 "<th>Remove</th></tr>"; 
-			System.out.print("hellosecond");
 			
 			String query = "SELECT * FROM `order`";
 			Statement stmt = con.createStatement(); 
 			ResultSet rs = stmt.executeQuery(query); 
 			
-			System.out.print("helloThird");
+			//iterate through the rows which taking from the table using result set
 			while(rs.next()) {
 				String orderId = Integer.toString(rs.getInt("orderId"));
 				String pid = Integer.toString(rs.getInt("pid"));
 				String researcherId = Integer.toString(rs.getInt("researcherId"));
-				String date = rs.getString("date"); 
-//			    Time timeObj = rs.getTime("time");
-//			    String time = timeObj.toString();
+				String date = rs.getString("date");
 				String time = rs.getString("time");
 			    String totAmount = Float.toString(rs.getFloat("totAmount"));
 			    String status = rs.getString("status");
 			
+			    //fill table using 
 				output += "<tr><td>"+ pid + "</td>";
 				output += "<td>"+ date + "</td>";
 				output += "<td>"+ time + "</td>";
 				output += "<td>"+ totAmount + "</td>";
 				output += "<td>"+ status + "</td>";
 				
-				System.out.println(pid);
-				System.out.println(orderId);
-				System.out.println(time);
-				System.out.println(totAmount);
-				
+				//implement buttons for the view also bind table's id
 				output += "<td><input name = 'btnUpdate' type = 'button' value = 'Update' class='btn btn-secondary'></td>"
 				+ "<td><form method = 'post' action = 'items.jsp'>"
 				+ "<input name='btnRemove' type = 'submit' value = 'Delete' class='btn btn-danger'>"
@@ -126,6 +123,7 @@ public class Product {
 		 
 	}
 	
+	//implement update ordered products in the cart
 	public String updateCart(String orderId, String pid,String researcherId,String date,String time,String totAmount,String status) 
 	{
 		String output = "";
@@ -137,7 +135,7 @@ public class Product {
 			if(con == null) {
 				return "Error while connecting to the database";
 			}
-			
+			//create prepared statement
 			String query = "Update `order` SET pid=? ,researcherId=? ,date=STR_TO_DATE(?,'%Y/%m/%d') ,time=STR_TO_DATE(?,'%T') ,totAmount=? ,status=? Where orderId=?";
 			PreparedStatement stmt = con.prepareStatement(query);
 			
@@ -148,7 +146,7 @@ public class Product {
 			stmt.setFloat(5,Float.parseFloat(totAmount));
 			stmt.setString(6,status);
 			stmt.setInt(7,Integer.parseInt(orderId));
-			
+			//execution statement
 			stmt.execute();
 			con.close();
 			output = "Updated successfully";
@@ -162,7 +160,7 @@ public class Product {
 		return output;
 	}
 	
-	
+	//implement delete ordered products from cart
 	public String deleteItem(String orderId) 
 	 { 
 		String output = ""; 
@@ -193,6 +191,7 @@ public class Product {
 	 	return output; 
 	 }
 	
+	//implement display order method for the relevant user
 	public String viewSelectCart(int orderId) {
 			
 			String output = "";
@@ -200,11 +199,10 @@ public class Product {
 				Connection con =  connect();
 				System.out.print(con);
 				if(con == null) { 
-					System.out.print("hello");
 					return "Error while connecting to the database for reading.";
 					
 				}
-				System.out.print("hellofirst");
+				//create html table's view structure
 				output = "<table border ='1'><tr><th>projectID</th>" +
 						 "<th>date</th>" +
 						 "<th>time</th>" +
@@ -212,35 +210,29 @@ public class Product {
 						 "<th>status</th>" +
 						 "<th>Update</th>" +
 						 "<th>Remove</th></tr>"; 
-				System.out.print("hellosecond");
+				//create prepared statement
 				String query = "SELECT * FROM `order` where orderId=" +orderId ;
 				Statement stmt = con.createStatement(); 
 				ResultSet rs = stmt.executeQuery(query); 
 				
 				
-				System.out.print("helloThird");
 				while(rs.next()) {
 					String orderId1 = Integer.toString(rs.getInt("orderId"));
 					String pid = Integer.toString(rs.getInt("pid"));
 					String researcherId = Integer.toString(rs.getInt("researcherId"));
-					String date = rs.getString("date"); 
-	//			    Time timeObj = rs.getTime("time");
-	//			    String time = timeObj.toString();
+					String date = rs.getString("date");
 					String time = rs.getString("time");
 				    String totAmount = Float.toString(rs.getFloat("totAmount"));
 				    String status = rs.getString("status");
 				
+				    //fill table which using above binding variables
 					output += "<tr><td>"+ pid + "</td>";
 					output += "<td>"+ date + "</td>";
 					output += "<td>"+ time + "</td>";
 					output += "<td>"+ totAmount + "</td>";
 					output += "<td>"+ status + "</td>";
 					
-					System.out.println(pid);
-					System.out.println(orderId);
-					System.out.println(time);
-					System.out.println(totAmount);
-					
+					//implement buttons
 					output += "<td><input name = 'btnUpdate' type = 'button' value = 'Update' class='btn btn-secondary'></td>"
 					+ "<td><form method = 'post' action = 'items.jsp'>"
 					+ "<input name='btnRemove' type = 'submit' value = 'Delete' class='btn btn-danger'>"
@@ -248,15 +240,13 @@ public class Product {
 					+ "</form></td></tr>";
 				}
 					con.close();
-					output += "</table>";
-								
+					output += "</table>";							
 			}
 			catch(Exception e) {
 				output = "Occure error while reading products";
 				System.err.println(e.getMessage()); 
 			}
 			
-			return output;
-			 
+			return output;			 
 		}
 }
